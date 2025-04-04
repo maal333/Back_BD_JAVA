5819c8b89ad4ac93dc494bab0adf57edb0d28c1e
Maal333 <mauropp3476@gmail.com>
2025-04-04 07:49:44 -0500

    carga_mauro

--- classroomapi/src/main/java/com/example/classroomapi/repositorios/IDocentRepository.java
+++ classroomapi/src/main/java/com/example/classroomapi/repositorios/IDocentRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IDocentRepository extends JpaRepository<Docent,Integer> {
 
-    //si tengo consultas personalizadas deben ir en este espacion
+    //si tengo consultas personalizadas deben ir en este espacion hola
 }
